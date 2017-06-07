e9c478b11c4cb152a571761e64c4ba8a82545f4c
Ezra Obiwale <contact@ezraobiwale.com>
2017-06-07 14:06:37 +0100

    Updated paginating with query for EventResource and CollectionResource with their respective config.json files

--- resources/paginate/get.js
+++ resources/paginate/get.js
@@ -13,18 +13,32 @@ delete query.page;
 // limit recursion
 query.$limitRecursion = 6;
 // check if should parse query
-if (query.query && ctx.getConfig('query.' + parts[0])) {
-    var queried = false;
-    ctx.getConfig('query.' + parts[0])
+if (query.query) {
+    var queried = false,
+            queryKeys = ctx.getConfig('queryKeys', null, parts[0]);
+    if (queryKeys) {
+        // check for EventResource parts
+        if (parts[1] && queryKeys[parts[1]]) queryKeys = queryKeys[parts[1]];
+        // queryKeys must be an array
+        if (Array.isArray(queryKeys) && queryKeys.length) {
+            query.$or = [];
             // loop over each key
-            .forEach(function (key) {
-                query[key] = {
+            queryKeys.forEach(function (key) {
+                var obj = {};
+                // set the query for key with regex
+                obj[key] = {
                     $regex: query.query,
                     $options: "i"
                 };
+                // add to the or query key
+                query.$or.push(obj);
+                // mark as queried
                 queried = true;
             });
-    if (queried) delete query.query;
+            // delete query key if already used
+            if (queried) delete query.query;
+        }
+    }
 }
 // get desired target
 dpd[colName].get(query, setResult);
